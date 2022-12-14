0d5b6385518705a335fdf5af060e4102e2f84b88
aisys-h-yamaguchi <hiroki.yamaguchi@aisysnet.com>
2022-12-15 00:00:55 +0900

    vuetify install

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+          v-model="drawer"
+          app
+        >
+      <!--  -->
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+    </v-main>
+  </v-app>
+</template>
 <script setup>
   import { Authenticator } from "@aws-amplify/ui-vue";
   import "@aws-amplify/ui-vue/styles.css";
@@ -7,12 +30,13 @@
 
   Amplify.configure(awsconfig);
 </script>
+<script>
 
-<template>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator>
-</template>
+export default {
+
+  data: () => ({
+    //
+    drawer: null
+  }),
+}
+</script>
